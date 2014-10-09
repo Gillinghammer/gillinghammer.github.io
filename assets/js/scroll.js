b8cdfd6accd8f954bdeddc0733a1c63c7a345275
Gillinghammer <colingillingham@gmail.com>
2014-10-10 00:16:23 +0100

    added animations to resume

---
+++ assets/js/scroll.js
@@ -0,0 +1,19 @@
+$(window).load(function(){
+  $("#resume_cv #headline h2").addClass("animated pulse");
+});
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    
+    if (scroll >= 100 ) {
+      $("#general_assembly").addClass("animated fadeInRightBig show");
+    }
+    if (scroll >= 400) {
+      $("#haystack").addClass("animated fadeInLeftBig show");
+    }
+    if (scroll >= 700) {
+      $("#tesla_motors").addClass("animated fadeInRightBig show");
+    }
+    if (scroll >= 1000) {
+      $("#skills_and_education").addClass("animated zoomInDown show");
+    }
+});
