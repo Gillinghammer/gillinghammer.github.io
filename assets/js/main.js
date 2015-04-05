ad6963edf2e8f76dc154cc436a6e6a075a2a8e4d
Gillinghammer <colingillingham@gmail.com>
2015-04-05 15:30:30 -0700

    added blogs section and updated nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,22 @@ $(document).ready(function() {
 
   // Nav slide down to fixed element
   $("nav").before($("nav").clone().addClass("animateIt"));
-      $(window).on("scroll", function () {
-          $("body").toggleClass("down", ($(window).scrollTop() > 100));
-      });
+    $(window).on("scroll", function () {
+      $("body").toggleClass("down", ($(window).scrollTop() > 100));
+  });
 
 
   // Carousel settings
+  $('.blog-carousel').slick({
+  infinite: true,
+  speed: 2500,
+  autoplay: true,
+  slidesToShow: 1,
+  arrows: false
+});
+
+
+
   $('.carousel').slick({
     speed: 2500,
     adaptiveHeight: false,
