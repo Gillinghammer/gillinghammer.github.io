8d785c208a4dd6bee1cce9fbeef2ec2728363ddf
Gillinghammer <colingillingham@gmail.com>
2015-06-18 10:49:13 -0700

    new look and feel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,27 @@
 $(document).ready(function() {
+  var client = new ZeroClipboard( $("img") );
+  client.on( "copy", function (event) {
+    var clipboard = event.clipboardData;
+    clipboard.setData( "text/plain", event.target.parentNode.childNodes[5].getAttribute('data-code') );
+    console.log(event.target.parentNode.childNodes[5].getAttribute('data-code'))
+  });
 
-    $( "#menu" ).click(function() {
-      $( '#navigation' ).slideUp("fast", function() {
-        $('#menuClosed').removeClass('hide');
-      });
-    });
-
-    $( "#menuClosed" ).click(function() {
-      $( '#navigation' ).slideDown("fast", function() {
-        $('#menuClosed').addClass('hide');
-        $('#navigation').removeClass('hide');
-      });
-    });
-
+  $('.referral img').hover(
+      function(e){
+        $(e.currentTarget.parentNode.childNodes[3]).removeClass("flipInX");
+        $(e.currentTarget.parentNode.childNodes[5]).removeClass("flipOutX");
+        $(e.currentTarget.parentNode.childNodes[3]).addClass("flipOutX");
+        $(e.currentTarget.parentNode.childNodes[3]).addClass("hidden");
+        $(e.currentTarget.parentNode.childNodes[5]).removeClass("hidden");
+        $(e.currentTarget.parentNode.childNodes[5]).addClass("flipInX");
+        setTimeout(function() {
+            $(e.currentTarget.parentNode.childNodes[3]).removeClass("flipOutX");
+            $(e.currentTarget.parentNode.childNodes[3]).removeClass("hidden");
+            $(e.currentTarget.parentNode.childNodes[3]).addClass("flipInX");
+            $(e.currentTarget.parentNode.childNodes[5]).removeClass("flipInX");
+            $(e.currentTarget.parentNode.childNodes[5]).addClass("flipOutX");
+            $(e.currentTarget.parentNode.childNodes[5]).addClass("hidden");
+        }, 4000);
+      }
+  )
 });
