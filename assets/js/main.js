22608b838815bca7054246143ff8e41f9ed23b6e
Gillinghammer <colingillingham@gmail.com>
2015-06-18 13:28:15 -0700

    updated css

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,23 @@ $(document).ready(function() {
   var client = new ZeroClipboard( $("img") );
   client.on( "copy", function (event) {
     var clipboard = event.clipboardData;
+    var code = event.target.parentNode.childNodes[5].getAttribute('data-code')
     clipboard.setData( "text/plain", event.target.parentNode.childNodes[5].getAttribute('data-code') );
+    $('#alertbar').removeClass("fadeOutUp");
+    $('#alertbar').removeClass("invisible");
+    $('#alertbar').addClass("fadeInDown");
+    $('#alertbar span').html("Copied URL to Clipboard!")
+    setTimeout(function() {
+      $('#alertbar').removeClass("fadeInDown");
+      $('#alertbar').addClass("fadeOutUp");
+    }, 2000)
+    
     console.log(event.target.parentNode.childNodes[5].getAttribute('data-code'))
   });
 
   $('.referral img').hover(
       function(e){
+        $(e.currentTarget).addClass("darken");
         $(e.currentTarget.parentNode.childNodes[3]).removeClass("flipInX");
         $(e.currentTarget.parentNode.childNodes[5]).removeClass("flipOutX");
         $(e.currentTarget.parentNode.childNodes[3]).addClass("flipOutX");
@@ -15,6 +26,7 @@ $(document).ready(function() {
         $(e.currentTarget.parentNode.childNodes[5]).removeClass("hidden");
         $(e.currentTarget.parentNode.childNodes[5]).addClass("flipInX");
         setTimeout(function() {
+          $(e.currentTarget).removeClass("darken");
             $(e.currentTarget.parentNode.childNodes[3]).removeClass("flipOutX");
             $(e.currentTarget.parentNode.childNodes[3]).removeClass("hidden");
             $(e.currentTarget.parentNode.childNodes[3]).addClass("flipInX");
