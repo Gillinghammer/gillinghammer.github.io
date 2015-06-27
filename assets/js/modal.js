93425f40b85d90d2afa8728dc93a91486784cc64
Gillinghammer <colingillingham@gmail.com>
2015-06-26 22:55:13 -0700

    fixed bug in safari

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
   $('#photos').on( "click", ".pure-img" , function(event) {
-    console.log("blah", event.currentTarget.parentNode.childNodes[1].innerHTML);
-    var img_src = event.currentTarget.currentSrc;
+    console.log("blah", $(event.currentTarget).attr("src"));
+    var img_src = $(event.currentTarget).attr("src");
     var img_location = event.currentTarget.parentNode.childNodes[1].innerHTML;
     var img_caption = event.currentTarget.parentNode.lastChild.innerText;
 
-    var div_content = "<div><img src='" + img_src + "'/>" +
+    var div_content = '<div><img src="' + img_src + '"/>' +
                       "<h2>" + img_location + "</h2>" +
                       "<p>" + img_caption + "</p></div>"
     vex.open({
