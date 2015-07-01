c9b531940649b57a127cfb0b2be7c36da689f0f1
Gillinghammer <colingillingham@gmail.com>
2015-07-01 05:28:08 -0500

    updated styling

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
 
   $('#photos').on( "click", ".pure-img" , function(event) {
-    // console.log("blah", $(event.currentTarget).attr("src"));
     var img_src = $(event.currentTarget).attr("src");
     var img_location = event.currentTarget.parentNode.childNodes[1].innerHTML;
     var img_caption = event.currentTarget.parentNode.lastChild.innerText;
-
-    var div_content = '<div><img src="' + img_src + '"/>' +
+    var div_content = '<div class="photoBox"><img src="' + img_src + '"/>' +
                       "<h2>" + img_location + "</h2>" +
                       "<p>" + img_caption + "</p></div>"
     vex.open({
@@ -15,4 +13,15 @@ $(document).ready(function() {
     mixpanel.track("Instgram Photo Clicked", {"img_src": img_src});
   });
   
+  $('.blogLink').click(function(e) {
+    e.preventDefault();
+    $.get($(e.currentTarget).attr("href")).complete(function(res) {
+      html_content = res.responseText;
+      console.log(html_content);
+      vex.open({
+        content: html_content
+      });
+    });
+  });
+
 });
